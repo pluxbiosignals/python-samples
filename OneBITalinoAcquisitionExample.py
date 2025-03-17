ae61b72f9e9575ce57f28c913cb8d1c39be59ee6
Guilherme Ramos <gramos@plux.info>
2025-03-17 16:01:53 +0000

    Inclusion of a BITalino use-case scenario

---
+++ OneBITalinoAcquisitionExample.py
@@ -0,0 +1,68 @@
+import platform
+import sys
+
+osDic = {
+    "Darwin": f"MacOS/Intel{''.join(platform.python_version().split('.')[:2])}",
+    "Linux": "Linux64",
+    "Windows": f"Win{platform.architecture()[0][:2]}_{''.join(platform.python_version().split('.')[:2])}",
+}
+if platform.mac_ver()[0] != "":
+    import subprocess
+    from os import linesep
+
+    p = subprocess.Popen("sw_vers", stdout=subprocess.PIPE)
+    result = p.communicate()[0].decode("utf-8").split(str("\t"))[2].split(linesep)[0]
+    if result.startswith("12."):
+        print("macOS version is Monterrey!")
+        osDic["Darwin"] = "MacOS/Intel310"
+        if (
+            int(platform.python_version().split(".")[0]) <= 3
+            and int(platform.python_version().split(".")[1]) < 10
+        ):
+            print(f"Python version required is ≥ 3.10. Installed is {platform.python_version()}")
+            exit()
+
+
+sys.path.append(f"PLUX-API-Python3/{osDic[platform.system()]}")
+
+import plux
+
+
+class NewDevice(plux.SignalsDev):
+    def __init__(self, address):
+        plux.SignalsDev.__init__(address)
+        self.duration = 0
+        self.frequency = 0
+
+    def onRawFrame(self, nSeq, data):  # onRawFrame takes three arguments
+        if nSeq % 2000 == 0:
+            print(nSeq, *data)
+        return nSeq > self.duration * self.frequency
+
+
+# example routines
+
+
+def exampleAcquisition(
+    address="BTH00:07:80:4D:2E:76",
+    duration=20,
+    frequency=1000,
+    active_ports=[1, 2, 3, 4, 5, 6],
+):  # time acquisition for each frequency
+    """
+    Example acquisition.
+    """
+    device = NewDevice(address)
+    device.duration = int(duration)  # Duration of acquisition in seconds.
+    device.frequency = int(frequency)  # Samples per second.
+    
+    # Trigger the start of the data recording: https://www.downloads.plux.info/apis/PLUX-API-Python-Docs/classplux_1_1_signals_dev.html#a028eaf160a20a53b3302d1abd95ae9f1
+    device.start(device.frequency, active_ports, 16)
+    device.loop()  # calls device.onRawFrame until it returns True
+    device.stop()
+    device.close()
+
+
+if __name__ == "__main__":
+    # Use arguments from the terminal (if any) as the first arguments and use the remaining default values.
+    exampleAcquisition(*sys.argv[1:])
